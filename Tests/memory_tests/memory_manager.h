4668ed39c9683c381fa4bac1a2b28d3e210ddc02
TomasBalbo <tbalboakoolen@itba.edu.ar>
2024-09-23 02:16:43 -0300

    TestMemManager

---
+++ Tests/memory_tests/memory_manager.h
@@ -0,0 +1,14 @@
+//
+// Created by Santiago Devesa on 21/09/2024.
+//
+
+#ifndef UNTITLED_MEMORY_MANAGER_H
+#define UNTITLED_MEMORY_MANAGER_H
+
+#include <stdint.h>
+
+void my_mem_init(void * m, uint32_t s);
+void * my_malloc(uint32_t size);
+void my_free(void * ptr);
+
+#endif //UNTITLED_MEMORY_MANAGER_H
--- Tests/memory_tests/mmTest.c
+++ Tests/memory_tests/mmTest.c
@@ -2,13 +2,12 @@
 // Created by Santiago Devesa on 21/09/2024.
 //
 
-#include "../include/test_util.h"
-#include "../include/memory_manager.h"
+#include "test_util.h"
+#include "memory_manager.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <syscall_lib.h>
 
 #define MAX_BLOCKS 128
 
@@ -17,9 +16,15 @@ typedef struct MM_rq {
     uint32_t size;
 } mm_rq;
 
+uint64_t test_mm(uint64_t argc, char *argv[]);
+
+int main(void) {
+    char *args[] = {"131072"};
+    return test_mm(1, args);
+}
+
 uint64_t test_mm(uint64_t argc, char *argv[]) {
 
-    int count = 0;
     mm_rq mm_rqs[MAX_BLOCKS];
     uint8_t rq;
     uint32_t total;
@@ -31,8 +36,10 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
     if ((max_memory = satoi(argv[0])) <= 0)
         return -1;
 
-    while (1) {
+    void *start = malloc(max_memory);
+    my_mem_init(start, max_memory);
 
+    while (1) {
         rq = 0;
         total = 0;
 
@@ -57,7 +64,7 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
         for (i = 0; i < rq; i++)
             if (mm_rqs[i].address)
                 if (!memcheck(mm_rqs[i].address, i, mm_rqs[i].size)) {
-                    sys_write(1,"test_mm ERROR", strlen("test_mm ERROR"),0x00FFFFFF);
+                    printf("test_mm ERROR");
                     return -1;
                 }
 
@@ -65,6 +72,5 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
         for (i = 0; i < rq; i++)
             if (mm_rqs[i].address)
                 my_free(mm_rqs[i].address);
-
     }
 }
